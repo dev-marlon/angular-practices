3f6f61d30b856e57e92c71b6671509887f27adc3
Marlon Hänsdieke <marlon.haensdieke@check24.de>
2020-11-13 12:54:14 +0100

    Create project with specified ng version

--- create.js
+++ create.js
@@ -1,12 +1,18 @@
 const { exec } = require('child_process');
-const projectName = process.argv.slice(2)[0];
+const ngVersion = process.argv.slice(2)[0];
+const projectName = process.argv.slice(2)[1];
+
+if (ngVersion === undefined) {
+    console.log('Specify the angular version.');
+    return;
+}
 
 if (projectName === undefined) {
     console.log('Project name missing.');
     return;
 }
 
-const command = `ng new ${projectName} --minimal --skipGit=true --style=less --routing=false`;
+const command = `npx -p @angular/cli@${ngVersion} ng new ${projectName} --minimal --skipGit=true --style=less --routing=false`;
 
 exec(command, (error, stdout, stderr) => {
     if (error) {
